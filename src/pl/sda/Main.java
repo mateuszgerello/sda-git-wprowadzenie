8aa43bc807b8733a393c24fffc5e7a007c8f190f
Mateusz Gerello <mateusz.gerello@gmail.com>
2018-10-06 15:52:06 +0200

    Dodanie komendy

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String ... args) {
         System.out.println("hdvvfubeijfhehirf"+getMsg());
+        System.out.println("hdvvfubeijfhehirf"+getMsg());
     }
     private static String getMsg(){
         return "testujemy GITa 2";
