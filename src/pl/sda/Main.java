e6381b9d28110103f8c6098ae84479402e89d2b1
Mateusz Gerello <mateusz.gerello@gmail.com>
2018-10-06 13:13:30 +0200

    add metchod getMag

--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -3,7 +3,7 @@ package pl.sda;
 public class Main {
 
     public static void main(String ... args) {
-        System.out.println("dddd"+getMsg());
+        System.out.println(""+getMsg());
     }
     private static String getMsg(){
         return "testujemy GITa";
